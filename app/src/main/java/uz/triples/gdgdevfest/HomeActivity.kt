3b8b5bdf6db465085919390c00a9af0c205de3c1
Sokhibjon Safarov <safarovsoxibjon18@gmail.com>
2020-09-26 09:16:19 +0500

    Firebase setup

---
+++ app/src/main/java/uz/triples/gdgdevfest/HomeActivity.kt
@@ -0,0 +1,18 @@
+package uz.triples.gdgdevfest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.triples.gdgdevfest.ui.main.HomeFragment
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, HomeFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/uz/triples/gdgdevfest/viewModels/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package uz.triples.gdgdevfest.viewModels
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
